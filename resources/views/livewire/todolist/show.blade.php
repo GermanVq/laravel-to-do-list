6b86faf11bb7755dcc7b932a7b91fd7fdb0ad969
GermanVq <gavegaq@gmail.com>
2024-04-07 11:00:17 -0500

    livewire tasks component added

---
+++ resources/views/livewire/todolist/show.blade.php
@@ -0,0 +1,15 @@
+<div>
+
+    <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+        <x-application-logo class="block h-12 w-auto" />
+
+        <h1 class="mt-8 text-2xl font-medium text-gray-900">
+            Crear y completa tus tareas!
+        </h1>
+        <div class="content">
+
+        </div>
+
+    </div>
+
+</div>
